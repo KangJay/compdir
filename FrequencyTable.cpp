4b8c2bf21261816f54575035c00dac302e31c5c3
Ji-Hoon Kang <jkang18@uw.edu>
2021-01-13 23:39:21 -0800

    refactored

--- FrequencyTable.cpp
+++ FrequencyTable.cpp
@@ -6,7 +6,6 @@ using namespace std;
 FrequencyTable::FrequencyTable(string filename) { 
   ifstream file(filename, ios::binary); 
   char input;
-  unsigned char index; 
   while(file.get(input)) {
     char_freqs[(unsigned char)input]++;
     totalBytes++; 
